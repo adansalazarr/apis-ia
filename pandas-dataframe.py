1bd74b734308f74c503e1daae61413a27ef63c89
adansalazarr <89493759+adansalazarr@users.noreply.github.com>
2023-09-25 20:52:38 -0600

    add pandas demos

--- pandas-dataframe.py
+++ pandas-dataframe.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 df = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
 print(df)
+
+df1 = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+print(df1)
+
+df2 = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 
+              'Sue': ['Pretty good.', 'Bland.']},
+             index=['Product A', 'Product B'])
+print(df2)
+
+df3 = pd.Series([1, 2, 3, 4, 5])
+print(df3)
